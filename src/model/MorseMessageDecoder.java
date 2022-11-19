fa023e87472cda40f056bfa3c8a7a8ad5241be7f
Christopher-Nottingham <cnott729@mtroyal.ca>
2022-11-18 21:54:10 -0700

    WIP

--- src/model/MorseMessageDecoder.java
+++ src/model/MorseMessageDecoder.java
@@ -4,5 +4,16 @@ import java.io.FileNotFoundException;
 
 public class MorseMessageDecoder
 {
+
+//  public String decodeMessage(String message) {
+//    // TODO Auto-generated method stub
+//    String decodedMsg;
+//    message.split("       ");
+//    for (int i = 0; i<=message.length();i++) {
+//      
+//    }
+//    return decodedMsg;
+//  }
+//  
     
 }
